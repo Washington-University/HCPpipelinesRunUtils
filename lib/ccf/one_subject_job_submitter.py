a45881f3ee1e65ddcfa60ee0b57d938a1e54661e
Junil Chang <junilc@wustl.edu>
2018-10-03 10:47:33 -0500

    changed path to find the start time file

--- lib/ccf/one_subject_job_submitter.py
+++ lib/ccf/one_subject_job_submitter.py
@@ -440,6 +440,12 @@ class OneSubjectJobSubmitter(abc.ABC):
 		module_logger.debug(debug_utils.get_name())
 		starttime_file_name = self.working_directory_name
 		starttime_file_name += os.path.sep
+		
+		starttime_file_name +=self.subject
+		starttime_file_name += os.path.sep
+		starttime_file_name +='ProcessingInfo'
+		starttime_file_name += os.path.sep
+		
 		starttime_file_name += self.PIPELINE_NAME
 		starttime_file_name += '.starttime'
 		return starttime_file_name
